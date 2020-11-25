d54bdaec2b83f5c265bdbaeaf8ca38179c7fc52a
wanjawischmeier <69650445+wanjawischmeier@users.noreply.github.com>
2020-11-25 16:32:13 +0100

    src dir

--- flat.js
+++
@@ -1,1 +0,0 @@
-javascript:(function(e){e.setAttribute("src","JS-URL-to-inject");document.getElementsByTagName("body")[0].appendChild(e);})(document.createElement("script"));void(0);
---
+++ src/flat.js
@@ -0,0 +1,1 @@
+javascript:(function(){varjsCode=document.createElement('script');jsCode.setAttribute('src','http://path/to/external/file.js');document.body.appendChild(jsCode);}());
